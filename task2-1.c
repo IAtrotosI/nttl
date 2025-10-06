257be1f55ddb95b1ba07797c58f4e79cd059bdd1
IAtrotosI <oleg2007836@gmail.com>
2025-10-06 14:48:30 +0300

    UpdateTwo task2-1.c

--- task2-1.c
+++ task2-1.c
@@ -1,6 +1,8 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <locale.h>
+#include <stdlib.h>
+#include <math.h>
 
 /**
 * @brief Перечислимый тип для выбора порядка сортировки.
@@ -12,35 +14,22 @@ typedef enum
 } SortOrder;
 
 /**
-* @brief Структура для хранения трех чисел.
-*/
-typedef struct {
-    double a;
-    double b;
-    double c;
-    int success; // Флаг успешного ввода
-} Numbers;
-
-/**
 * @brief Функция сортировки трех чисел методом пузырька.
 * @param a - Первое число.
 * @param b - Второе число.
 * @param c - Третье число.
 * @param order - Порядок сортировки (increase или descending).
+* @param minv - Минимальное из чисел.
+* @param midv - Среднее из чисел.
+* @param maxv - Максимальное из чисел.
 */
-void sortNumbers(double* a, double* b, double* c, SortOrder order);
-
-/**
-* @brief Функция считывания выбора порядка сортировки с проверкой ввода.
-* @return Возвращает выбранный порядок сортировки, или 0 при ошибке.
-*/
-SortOrder getSortOrder();
+void sort_and_print_Numbers(const double a, const double b, const double c, SortOrder order);
 
 /**
 * @brief Функция считывания трех чисел с клавиатуры с проверкой ввода.
 * @return Возвращает структуру Numbers с числами и флагом успеха.
 */
-Numbers getValues();
+int getValue();
 
 /**
 * @brief Точка входа в программу.
@@ -50,83 +39,48 @@ Numbers getValues();
 */
 int main(void) {
     char* locale = setlocale(LC_ALL, "");
-
-    Numbers numbers = getValues();
-    if (!numbers.success) {
-        return 1;
-    }
-    SortOrder order = getSortOrder();
-    if (order != increase && order != descending) {
-        printf("Ошибка выбора порядка сортировки!\n");
-        return 1;
-    }
-
-    sortNumbers(&numbers.a, &numbers.b, &numbers.c, order);
-
-    // Выводим результат в зависимости от выбора пользователя.
-    if (order == increase) {
-        printf("Числа в порядке возрастания: %.2f, %.2f, %.2f\n", numbers.a, numbers.b, numbers.c);
-    }
-    else {
-        printf("Числа в порядке убывания: %.2f, %.2f, %.2f\n", numbers.a, numbers.b, numbers.c);
-    }
-
-    return 0;
-}
-
-Numbers getValues() {
-    Numbers nums;
-    nums.success = 0;
-
-    printf("Введите три числа a, b, c: ");
-    if (scanf("%lf %lf %lf", &nums.a, &nums.b, &nums.c) == 3) {
-        nums.success = 1; // Успешный ввод
-    }
-    else {
-        printf("Ошибка ввода! Требуется три вещественных числа.\n");
-    }
-
-    return nums;
-}
-
-SortOrder getSortOrder() {
-    int choice;
+    int a = getValue();
+    int b = getValue();
+    int c = getValue();
     printf("Выберите порядок сортировки:\n");
     printf("%d - по возрастанию\n", increase);
     printf("%d - по убыванию\n", descending);
     printf("Ваш выбор: ");
-
-    if (scanf("%d", &choice) != 1) {
-        printf("Ошибка ввода!\n");
-        return 0;
-    }
-
-    switch (choice)
-    {
-    case increase: return increase;
-    case descending: return descending;
-    default:
-        printf("Неверный выбор! Допустимые значения: %d или %d\n", increase, descending);
-        return 0;
-    }
+    int choice = getValue();
+    sort_and_print_Numbers(a, b, c, choice);
+    return 0;
 }
 
-void sortNumbers(double* a, double* b, double* c, SortOrder order) {
-    double temp;
-    // Сортировка методом пузырька для трех чисел
-    if ((order == increase && *a > *b) || (order == descending && *a < *b)) {
-        temp = *a;
-        *a = *b;
-        *b = temp;
+int getValue(void) 
+{
+    int num = 0;
+
+    printf("Введите число: ");
+    if (!scanf("%d", &num)) 
+    {
+        printf("Ошибка ввода!\n");
+        exit(1);
     }
-    if ((order == increase && *b > *c) || (order == descending && *b < *c)) {
-        temp = *b;
-        *b = *c;
-        *c = temp;
-    }
-    if ((order == increase && *a > *b) || (order == descending && *a < *b)) {
-        temp = *a;
-        *a = *b;
-        *b = temp;
+
+    return num;
+}
+
+
+void sort_and_print_Numbers(double a, double b, double c, int order)
+{
+    double minv = min(a, min(b, c));
+    double maxv = max(a, max(b, c));
+    double midv = (a + b + c) - minv - maxv;
+
+    switch (order)
+    {
+    case increase:
+        printf("Числа в порядке возрастания: %.2f, %.2f, %.2f\n", minv, midv, maxv);
+        break;
+    case descending:
+        printf("Числа в порядке убывания: %.2f, %.2f, %.2f\n", maxv, midv, minv);
+        break;
+    default:
+        printf("Неправильный выбор\n");
     }
 }
